16b89ece49b98c2dc347a5aee4c16e8cef127b7f
Alis Lin <5921000@qq.com>
2019-12-26 13:56:27 +0800

    add NotifyChanged

---
+++ Thunder.Standard.Lib/Model/NotifyChanged.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Thunder.Standard.Lib.Model
+{
+    public class NotifyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void PropertyUpdate(object sender, string PropertyName)
+        {
+            PropertyChanged?.Invoke(sender, new PropertyChangedEventArgs(PropertyName));
+        }
+
+    }
+}
